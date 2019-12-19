731d8728f0c5429c46d68c8e714a308e744d2e62
tvallet16 <t.vallet@it-students.fr>
2019-12-19 10:42:58 +0100

    test big screen

--- ecommerce/js/script.js
+++ ecommerce/js/script.js
@@ -2,7 +2,7 @@ function pseud (){
     let pseudo = document.getElementById("pseudo").value;
     console.log(pseudo);
     document.getElementById("formHeader").style.display="none";
-    alert(`bienvenu ${pseudo} profitez pleinement de notre site`);
+    alert(`Bienvenue ${pseudo}, profitez pleinement de notre site`);
 }
 
         
